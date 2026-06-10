5f4921e7dd7a282f3594c4222fe1f857a6812db2
Jannabitte <jannabitte.pino@edu.udla.cl>
2026-06-10 14:56:57 -0400

    Se crea  proyecto evaluacion modular con Spring Boot y esta vez con Gradle

---
+++ src/main/java/cl/vettask/api/VetTaskApiApplication.java
@@ -0,0 +1,13 @@
+package cl.vettask.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetTaskApiApplication.class, args);
+    }
+
+}
